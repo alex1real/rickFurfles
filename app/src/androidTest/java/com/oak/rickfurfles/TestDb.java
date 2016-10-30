8944b76af319797ac4dd6882eff2838a1ead9a03
Alex Carvalho <Alex Carvalho>
2016-10-30 19:46:31 +0000

    test: Lift Table creation and insertion

--- app/src/androidTest/java/com/oak/rickfurfles/TestDb.java
+++ app/src/androidTest/java/com/oak/rickfurfles/TestDb.java
@@ -8,6 +8,7 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.oak.rickfurfles.model.dm.LiftContract;
+import com.oak.rickfurfles.model.dm.LiftDbBaseColumns;
 import com.oak.rickfurfles.model.dm.LiftDbHelper;
 
 import junit.framework.Assert;
@@ -47,6 +48,7 @@ public class TestDb {
         // A List of tables to be checked if they were created
         HashSet<String> tableNamesHashSet = new HashSet<>();
         tableNamesHashSet.add(LiftContract.ShiftEntry.TABLE_NAME);
+        tableNamesHashSet.add(LiftContract.LiftEntry.TABLE_NAME);
 
 
         Context appContext = InstrumentationRegistry.getTargetContext();
@@ -79,19 +81,73 @@ public class TestDb {
     /*
      * It checks:
      *  - If the db could be opened
+     *  - If it was possible to insert a Shift Record in the db
+     *  - If it was possible to insert 2 Lift sample Records
+     *  - If the recorde inserted into Lift table are with the expected values
+     */
+    @Test
+    public void insertLift(){
+        // Open db (Create if it doesn't exist)
+        SQLiteDatabase sqLiteDatabase = this.openCreateWritableDb();
+
+        // Create a Shift
+        ContentValues shiftValues = this.getDefaultShiftCV();
+
+        long shiftId = sqLiteDatabase.insert(LiftContract.ShiftEntry.TABLE_NAME,
+                null,
+                shiftValues);
+
+        // Check if the Shift was created
+        Assert.assertTrue("insertLift Error: Failure to insert shift.", shiftId != -1);
+
+        // Create a Lift
+        ContentValues liftSampleValues1 = this.getSampleLiftValues1(shiftId);
+
+        long liftId1 = sqLiteDatabase.insert(LiftContract.LiftEntry.TABLE_NAME,
+                null,
+                liftSampleValues1);
+
+        // Check if the Lift was created
+        Assert.assertTrue("inserLift Error: Failure to insert Lift.", liftId1 != -1);
+
+        // Create another Lift
+        ContentValues liftSampleValues2 = this.getSampleLiftValues1(shiftId);
+
+        long liftId2 = sqLiteDatabase.insert(LiftContract.LiftEntry.TABLE_NAME,
+                null,
+                liftSampleValues2);
+
+        // Check if the Lift was created
+        Assert.assertTrue("insertLift Error: Failure to insert Lidt.", liftId2 != -1);
+
+        // Query all Lifts
+        Cursor cursor = sqLiteDatabase.rawQuery("select * from " + LiftContract.LiftEntry.TABLE_NAME,
+                null);
+
+        // Check if the first sample Lift is present in the cursor
+        liftSampleValues1.put(LiftContract.LiftEntry._ID, liftId1);
+
+        Assert.assertTrue("insertLift Error: Inconsistencies between cursor and expected sample values 1",
+                TestUtilities.isValidCursor(cursor, liftSampleValues1));
+
+        // Check if the second sample Lift is present in the cursor
+        liftSampleValues2.put(LiftContract.LiftEntry._ID, liftId2);
+
+        Assert.assertTrue("insertLift Error: Inconsistencies between cursor and expected sample values 2",
+                TestUtilities.isValidCursor(cursor, liftSampleValues2));
+
+        cursor.close();
+    }
+
+    /*
+     * It checks:
+     *  - If the db could be opened
      *  - If it was possible to insert a record in the db
      *  - If the record inserted into database is with the expected values
      */
     @Test
     public void InsertShift(){
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        LiftDbHelper liftDbHelper = new LiftDbHelper(appContext);
-        SQLiteDatabase sqLiteDatabase = liftDbHelper.getWritableDatabase();
-
-        // Check if it was possible to open the database
-        Assert.assertTrue("insertShift Error: It wasn't possible to open the db.",
-                sqLiteDatabase.isOpen());
+        SQLiteDatabase sqLiteDatabase = this.openCreateWritableDb();
 
         ContentValues shiftValues = this.getDefaultShiftCV();
 
@@ -142,6 +198,45 @@ public class TestDb {
     }
 
     /*
+     * It generates a ContentValues object for the Lift where:
+     *  - Start Date: 2 hours ago
+     *  - End Date: 1:46 min ago
+     *  - Price: 15
+     *  - Number of Passengers: 3
+     *  - Shift Id: @param
+     */
+    private ContentValues getSampleLiftValues1(long shiftId){
+        GregorianCalendar startDate = new GregorianCalendar();
+        startDate.add(Calendar.HOUR_OF_DAY, -2);
+
+        GregorianCalendar endDate = new GregorianCalendar();
+        endDate.add(Calendar.HOUR_OF_DAY, -1);
+        endDate.add(Calendar.MINUTE, -46);
+
+        return this.getLiftValues(startDate, endDate, 15, 3, shiftId);
+    }
+
+    /*
+     * It generates a ContentValues object for the Lift where:
+     *  - Start Date: 1:30 hours ago
+     *  - End Date: 1:23 min ago
+     *  - Price: 12
+     *  - Number of Passangers: 2
+     *  - Shift Id: @param
+     */
+    private ContentValues getSampleLiftValues2(long shiftId){
+        GregorianCalendar startDate = new GregorianCalendar();
+        startDate.add(Calendar.HOUR_OF_DAY, -1);
+        startDate.add(Calendar.MINUTE, -30);
+
+        GregorianCalendar endDate = new GregorianCalendar();
+        endDate.add(Calendar.HOUR_OF_DAY, -1);
+        endDate.add(Calendar.MINUTE, - 37);
+
+        return this.getLiftValues(startDate, endDate, 12, 2, shiftId);
+    }
+
+    /*
      * Generate a ContentValues object for the shift where:
      *  - Creation Date: Current Date
      *  - Last Update Date: Current Date
@@ -158,6 +253,45 @@ public class TestDb {
         shiftValues.put(LiftContract.ShiftEntry.COLUMN_END_DT, endDate.getTimeInMillis());
 
         return shiftValues;
+    }
+
+    /*
+     * It generates a ContentValues for the Lift entity, where all data is passed as parameter.
+     */
+    private ContentValues getLiftValues(Calendar startDate,
+                                        Calendar endDate,
+                                        int price,
+                                        int passengers_num,
+                                        long shiftId){
+        GregorianCalendar creationDate = new GregorianCalendar();
+
+        ContentValues liftValues = new ContentValues();
+        liftValues.put(LiftContract.LiftEntry.COLUMN_CREATED, creationDate.getTimeInMillis());
+        liftValues.put(LiftContract.LiftEntry.COLUMN_LAST_UPD, creationDate.getTimeInMillis());
+        liftValues.put(LiftContract.LiftEntry.COLUMN_START_DT, startDate.getTimeInMillis());
+        liftValues.put(LiftContract.LiftEntry.COLUMN_END_DT, endDate.getTimeInMillis());
+        liftValues.put(LiftContract.LiftEntry.COLUMN_PRICE, 15);
+        liftValues.put(LiftContract.LiftEntry.COLUMN_PASSENGERS_NUM, 3);
+        liftValues.put(LiftContract.LiftEntry.COLUMN_SHIFT_ID, shiftId);
+
+        return liftValues;
+    }
+
+    /*
+     * - Open db (Create if it doesn't exist)
+     * - Check if it was possible to open the db
+     */
+    private SQLiteDatabase openCreateWritableDb(){
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        LiftDbHelper liftDbHelper = new LiftDbHelper(appContext);
+
+        SQLiteDatabase sqLiteDatabase = liftDbHelper.getWritableDatabase();
+
+        Assert.assertTrue("openCreateWritableDb Error: It wasn't possible to open the db.",
+                sqLiteDatabase.isOpen());
+
+        return sqLiteDatabase;
     }
 
 }
